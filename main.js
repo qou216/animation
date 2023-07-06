598331e9ebbc1f6dd448c14207f913dfb56acf27
=qou216 <qou216@naver.com>
2023-07-06 14:22:58 +0900

    second

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let mainText = document.querySelector("h1");
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     console.log("scrollY", value);
-    
 
-})
+    if(value>400){
+        mainText.style.animation="backSlide 1s ease-out forwards";
+    }else{
+        mainText.style.animation='slide 1s ease-out'
+    }
+
+});
